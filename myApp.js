76041877627aa534cd88d1076820d05e50dd1ef1
Elijah Wilsher <elijahwilsher@gmail.com>
2024-07-05 08:50:55 +0000

    added middleware for serving static files in /public

--- myApp.js
+++ myApp.js
@@ -3,9 +3,24 @@ let app = express();
 
 console.log('Hello World!')
 
+// Prints Hello express to page with path '/'
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Express')
+// })
+
+
+// Sends file as response when GET request given to '/' path 
+
+const absolutePath = __dirname + '/views/index.html'
+
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(absolutePath)
 })
+
+// Middleware function to serve static files in public folder 
+
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
